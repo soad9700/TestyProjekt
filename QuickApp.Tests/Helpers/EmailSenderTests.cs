e7276dcbd1710257c348e72bd6e456e67665a058
Filip Lipowiecki <lipowiecki.f@gmail.com>
2020-01-05 21:01:29 +0100

    AssignRolesAuthorizationHandlerTests

---
+++ QuickApp.Tests/Helpers/EmailSenderTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickApp.Tests.Helpers
+{
+    public class EmailSenderTests
+    {
+
+
+        [SetUp]
+        public void Setup()
+        {
+            
+        }
+    }
+}
