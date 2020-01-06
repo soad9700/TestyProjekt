97d2a38e6938f0c4053b1bec935ab1abdbeb9780
Filip Lipowiecki <lipowiecki.f@gmail.com>
2020-01-06 07:30:58 +0100

    MinimumCountAttributeTests

--- QuickApp.Tests/Helpers/EmailSenderTests.cs
+++ QuickApp.Tests/Helpers/EmailSenderTests.cs
@@ -1,18 +1,79 @@
-﻿using NUnit.Framework;
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moq;
+using NUnit.Framework;
+using QuickApp.Helpers;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace QuickApp.Tests.Helpers
 {
     public class EmailSenderTests
     {
+        private EmailSender SUT;
+        private IOptions<AppSettings> _config;
+        private Mock<ILogger<EmailSender>> _logger;
 
 
         [SetUp]
         public void Setup()
         {
-            
+            _config = Options.Create(new AppSettings());
+            _config.Value.SmtpConfig = new SmtpConfig();
+            _config.Value.SmtpConfig.Name = "testName";
+            _config.Value.SmtpConfig.EmailAddress = "testEmailAddress@gmail.com";
+            _logger = new Mock<ILogger<EmailSender>>();
+            SUT = new EmailSender(_config, _logger.Object);
         }
+
+        [Test]
+        public async Task SendEmailAsync_OverloadOne_InvokedWithHostNull_ShouldReturnErrorMessageInTuple()
+        {
+            var senderName = "testName";
+            var senderEmail = "testEmail@gmail.com";
+            var recepientName = "testName2";
+            var recepientEmail = "testEmail2@gmail.com";
+            var subject = "testSubject";
+            var body = "testEmailBody";
+
+            var expectedResult = (false, "Value cannot be null. (Parameter 'host')");
+
+            var result = await SUT.SendEmailAsync(senderName, senderEmail,
+                recepientName, recepientEmail, subject, body);
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [Test]
+        public async Task SendEmailAsync_InvokedWithHostNull_ShouldReturnErrorMessageInTuple()
+        {
+            var recepientName = "testName2";
+            var recepientEmail = "testEmail2@gmail.com";
+            var subject = "testSubject";
+            var body = "testEmailBody";
+
+            var expectedResult = (false, "Value cannot be null. (Parameter 'host')");
+
+            var result = await SUT.SendEmailAsync(recepientName, recepientEmail, subject, body);
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
+//        [Test]
+//        public async Task SendEmailAsync_InvokedWithHostNull_ShouldReturnErrorMessageInTuple()
+//        {
+//            var recepientName = "testName2";
+//            var recepientEmail = "testEmail2@gmail.com";
+//            var subject = "testSubject";
+//            var body = "testEmailBody";
+//
+//            var expectedResult = (false, "Value cannot be null. (Parameter 'host')");
+//
+//            var result = await SUT.SendEmailAsync(recepientName, recepientEmail, subject, body);
+//
+//            Assert.AreEqual(expectedResult, result);
+//        }
     }
 }
---
+++ QuickApp.Tests/Helpers/MinimumCountAttributeTests.cs
@@ -0,0 +1,83 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using QuickApp.Helpers;
+
+namespace QuickApp.Tests.Helpers
+{
+    public class MinimumCountAttributeTests
+    {
+        private MinimumCountAttribute SUT;
+        
+        
+        [SetUp]
+        public void Setup()
+        {
+            SUT = new MinimumCountAttribute();
+        }
+
+        [Test]
+        public void IsValid_WhenArgumentNullAndRequiredTrue_ShouldReturnFalse()
+        {
+            var result = SUT.IsValid(null);
+            Assert.AreEqual(false, result);
+        }
+        
+        [Test]
+        public void IsValid_WhenArgumentNullAndRequiredFalse_ShouldReturnTrue()
+        {
+            SUT = new MinimumCountAttribute(1, false);
+            var result = SUT.IsValid(null);
+            Assert.AreEqual(true, result);
+        }
+        
+        [Test]
+        public void IsValid_WhenArgumentNotNull_ShouldReturnTrue()
+        {
+            var testICollection = new List<string>(){"test1", "test2", "test3", "test4", "test5"};
+
+            var result = SUT.IsValid(testICollection);
+            Assert.AreEqual(true, result);
+        }
+        
+        [Test]
+        public void IsValid_WhenArgumentNotNullAndMinCountBiggerThanListCount_ShouldReturnFalse()
+        {
+            var testICollection = new List<string>(){"test1", "test2", "test3", "test4", "test5"};
+            
+            SUT = new MinimumCountAttribute(10);
+
+            var result = SUT.IsValid(testICollection);
+            Assert.AreEqual(false, result);
+        }
+        
+        [Test]
+        public void IsValid_WhenArgumentNotICollection_ShouldReturnFalse()
+        {
+            var testObject = new object();
+            
+            var result = SUT.IsValid(testObject);
+            Assert.AreEqual(false, result);
+        }
+
+        [Test]
+        public void FormatErrorMessage_WhenArgumentValid_ShouldReturnStringErrorMessageWithArgumentIncluded()
+        {
+            var testName = "testName";
+
+            var result = SUT.FormatErrorMessage(testName);
+            
+            Assert.That(result.Contains(testName));
+        }
+        
+        [Test]
+        public void FormatErrorMessage_WhenArgumentNull_ShouldReturnStringErrorMessageWithNoArgumentIncluded()
+        {
+            var emptyNameErrorMessageLength = 29;
+
+            var result = SUT.FormatErrorMessage(null);
+            
+            Assert.That(result.Length == emptyNameErrorMessageLength);
+        }
+        
+    }
+}
